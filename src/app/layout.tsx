f317ec268364a7b5bb54e2ef0d530e2fb8e648a6
devtrills <godwinikechukwu.dev@gmail.com>
2023-11-14 20:12:05 +0100

    loaded local fonts
    loaded local fonts through 'next/font/local'.
    
    and added missing font files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,70 @@
 import type { Metadata } from 'next'
 // import localFont from 'next/font/local'
-import { Poppins } from 'next/font/google'
+import { Poppins } from 'next/font/google';
+import localFont from "next/font/local";
 
 import './globals.css'
 import './line-awesome.css'
 import "rc-slider/assets/index.css";
 
 import Providers from "@/app/redux/provider";
+// import fontPath from "@/app/fonts"
 
 const poppins = Poppins({
-  weight: ['300','400','500','600'],
+  weight: ['300','400','500','600', '700'],
   subsets: ['latin'],
   variable: '--font-poppins',
 })
-// const myFont = localFont({
-//   src: [{
-//     path: '',
-//     weight: '400',
-//     style: 'normal',
-//   },{
-//     path: '',
-//     weight: '400',
-//     style: 'normal'}]
-// })
+const myFonts = localFont({
+  src: [
+    {
+    path: './fonts/la-brands-400.ttf',
+    weight: '400',
+    style: 'brands'
+  },
+  {
+    path: './fonts/la-brands-400.woff',
+    weight: '400',
+    style: 'brands'
+  },
+  {
+    path: './fonts/la-brands-400.woff2',
+    weight: '400',
+    style: 'brands'
+  },
+  {
+  path: './fonts/la-regular-400.ttf',
+  weight: '400',
+  style: 'regular'
+  },
+  {
+    path: './fonts/la-regular-400.woff',
+    weight: '400',
+    style: 'regular'
+  },
+  {
+    path: './fonts/la-regular-400.woff2',
+    weight: '400',
+    style: 'regular'
+  },
+  {
+  path: './fonts/la-solid-900.ttf',
+  weight: '900',
+  style: 'solid'
+  },
+  {
+  path: './fonts/la-solid-900.woff',
+  weight: '900',
+  style: 'solid'
+  },
+  {
+  path: './fonts/la-solid-900.woff2',
+  weight: '900',
+  style: 'solid'
+  },
+],
+variable: '--font-la'
+})
 
 export const metadata: Metadata = {
   title: 'TruthStore Commerce',
