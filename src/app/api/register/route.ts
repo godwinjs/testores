e20f161977d35a40f3b9068e18e07e3f6ae117c7
devtrills <godwinikechukwu.dev@gmail.com>
2023-11-20 01:15:16 +0100

    cleared cons..

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -6,16 +6,13 @@ import connect from "@/db/utils/connect";
 
 export const POST = async (request: any) => {
     const { fullName, email, password } = await request.json();
-
-    // console.log([fullName, email, password ])
     
     await connect();
     
     const hashedPassword = await bcrypt.hash(password, 5);
     // const existingUser: any = 
     const existingUser: any = await User.findOne({email: email});
-
-    console.log(!(existingUser === null))
+    
     if(!(existingUser === null)){
         return new NextResponse(`User with email: ${email} already exist.`, {status: 400})
     }
@@ -27,7 +24,7 @@ export const POST = async (request: any) => {
     })
 
     try {
-        console.log([fullName, email, hashedPassword])
+        // console.log([fullName, email, hashedPassword])
         await newUser.save();
         return new NextResponse("User registered", {status: 200})
     } catch (err: any) {
--- src/db/utils/connect.js
+++ src/db/utils/connect.js
@@ -4,7 +4,6 @@ const connect = async () => {
     if(mongoose.connections[0].readyState) return;
 
     try {
-        console.log(process.env.MONGO_URL) 
         await mongoose.connect(process.env.MONGO_URL)
 
         console.log("mongoose connection establised") 
