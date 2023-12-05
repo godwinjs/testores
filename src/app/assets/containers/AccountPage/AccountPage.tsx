f51785f9080bb824f34e02e816d4a5b29a94a09c
devtrills <godwinikechukwu.dev@gmail.com>
2023-12-05 18:57:46 +0100

    Update AccountPage.tsx
    Fixed preview

--- src/app/assets/containers/AccountPage/AccountPage.tsx
+++ src/app/assets/containers/AccountPage/AccountPage.tsx
@@ -165,7 +165,7 @@ const AccountPage: FC<AccountPageProps> = ({ className = "" }) => {
                   alt=""
                   cldImg={ProfilePicture}
                   className="w-32 h-32 rounded-full object-cover z-0"
-                />)) : <div className="w-32 h-32 bg-black rounded-full object-cover z-0"></div>}
+                />)) : (preview ? <img src={preview} alt="preview" className="w-32 h-32 rounded-full object-cover z-0" /> : <div className="w-32 h-32 bg-black rounded-full object-cover z-0"></div>)}
                 
                 <div className="group-hover:visible lg:invisible absolute inset-0 bg-black bg-opacity-60 flex flex-col items-center justify-center text-neutral-50 cursor-pointer">
                   <svg
