41434fa009355c54b8a6e879b202cb2b45aac9e6
devtrills <godwinikechukwu.dev@gmail.com>
2023-12-05 14:41:06 +0100

    Update connect.js

--- src/app/db/utils/connect.js
+++ src/app/db/utils/connect.js
@@ -7,7 +7,7 @@ const connect = async () => {
     if(mongoose.connections[0].readyState) return;
 
     try {
-        await mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URL)
+        await mongoose.connect(process.env.MONGO_URL)
         console.log("mongoose connection establised") 
     } catch(error) {
         throw new Error("Error Connection to mongoose")
