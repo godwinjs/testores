9ff8caa179e0b5d27aae221562fd0ca3b2078554
devtrills <godwinikechukwu.dev@gmail.com>
2023-12-26 12:22:24 +0100

    About Pages added

---
+++ src/app/(routes)/about/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+// import { RootState } from "@/app/redux/store";
+// import { useSelector, useDispatch } from "react-redux";
+// import { increment, decrement, increamentByAmount } from "@/app/redux/features/counter/counterSlice";
+import PageAbout from "@/app/assets/containers/PageAbout/PageAbout"
+
+export default function Page_About() {
+    // const count = useSelector((state: RootState) => state.counter.value);
+    // const dispatch = useDispatch();
+
+    return <PageAbout />
+}
