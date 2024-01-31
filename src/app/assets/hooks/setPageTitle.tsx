0ea9ca6f0aafcf372ff2d9987f9bd101dbf1ecdf
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-01-31 08:11:51 +0100

    Update setPageTitle.tsx

---
+++ src/app/assets/hooks/setPageTitle.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function SetPageTitle({ title } : { title: string }){
+
+    React.useEffect(() => {
+        document.title = title;
+    }, [title])
+
+    return null;
+}   
