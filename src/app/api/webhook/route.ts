845ac39e03b6969922528775e0e9280d917491ea
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-04-18 11:50:07 +0100

    Update route.ts

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,4 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from "next"
+import { NextApiRequest, NextApiResponse } from "next"
 import { NextResponse } from "next/server"
 const crypto = require('crypto');
 
