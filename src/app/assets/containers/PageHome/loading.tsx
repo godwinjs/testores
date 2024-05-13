855690f6d38eb924707a10ec873d590f7d9c8d87
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-05-13 14:48:13 +0100

    Delete loading.tsx

--- src/app/assets/containers/PageHome/loading.tsx
+++
@@ -1,6 +0,0 @@
-// import DualCircleLoader from "@/app/assets/components/Loaders/DualCircleLoader";
-
-// export default function Loading(){
-//     return <DualCircleLoader height={100} size={150} />
-//     // return "Loading..............................................................................................................................."
-// }
