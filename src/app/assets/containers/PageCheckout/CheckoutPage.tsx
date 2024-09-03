e1a7e3f70a4dd8c445578af9e24b02be4cb3c38e
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-09-03 01:52:28 +0100

    Update CheckoutPage.tsx

--- src/app/assets/containers/PageCheckout/CheckoutPage.tsx
+++ src/app/assets/containers/PageCheckout/CheckoutPage.tsx
@@ -366,7 +366,7 @@ const CheckoutPage = ({user}: any) => {
                 </span>
               </div>
             </div>
-            <ButtonPrimary onClick={handleSubmit} className="ttnc-ButtonPrimary disabled:cursor-not-allowed disabled:bg-opacity-90 bg-slate-900 dark:bg-slate-100 hover:bg-slate-800 text-slate-50 dark:text-slate-800 shadow-xl mt-8 w-full rounded py-4" > pay </ButtonPrimary>
+            {/* <ButtonPrimary onClick={handleSubmit} className="ttnc-ButtonPrimary disabled:cursor-not-allowed disabled:bg-opacity-90 bg-slate-900 dark:bg-slate-100 hover:bg-slate-800 text-slate-50 dark:text-slate-800 shadow-xl mt-8 w-full rounded py-4" > pay </ButtonPrimary> */}
             <PaystackButton className="ttnc-ButtonPrimary disabled:cursor-not-allowed disabled:bg-opacity-90 bg-slate-900 dark:bg-slate-100 hover:bg-slate-800 text-slate-50 dark:text-slate-800 shadow-xl mt-8 w-full rounded py-4" {...componentProps} />
             
             <div className="mt-5 text-sm text-slate-500 dark:text-slate-400 flex items-center justify-center">
