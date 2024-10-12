290fc3d1ee130a28156ed8fb0e17585c62c979e8
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-10-12 08:51:49 +0100

    Update page.tsx

--- src/app/(routes)/subscription/page.tsx
+++ src/app/(routes)/subscription/page.tsx
@@ -1,22 +1,17 @@
 "use client"
 
-import React, { FC } from "react";
+import React from "react";
 
 import SetPageTitle from "@/app/assets/hooks/SetPageTitle";
 import BgGlassmorphism from "@/app/assets/components/BgGlassmorphism/BgGlassmorphism";
-// import SectionClientSay from "@/app/assets/components/SectionClientSay/SectionClientSay";
 import SectionPromo3 from "@/app/assets/components/SectionPromo3";
 
-export interface PageSubscriptionProps {
-  className?: string;
-}
-
-const PageSubscription: FC<PageSubscriptionProps> = ({ className = "" }) => {
+export default function PageSubscription() {
   SetPageTitle({title: "Subscription || Truthstores"})
 
   return (
     <div
-      className={`nc-PageAbout overflow-hidden relative ${className}`}
+      className={`nc-PageAbout overflow-hidden relative`}
       data-nc-id="PageAbout"
     >
 
@@ -29,6 +24,4 @@ const PageSubscription: FC<PageSubscriptionProps> = ({ className = "" }) => {
       </div>
     </div>
   );
-};
-
-export default PageSubscription;
+}
